fc796f0918e85d1a0a2f88c976e4f7f45b2c06bd
Alexander Sarson <Alexander_Sarson@Outlook.com>
2020-05-07 14:22:22 +0200

    fixed ugerpage cypress test

--- cypress/e2e/userpage.spec.js
+++ cypress/e2e/userpage.spec.js
@@ -5,8 +5,8 @@ describe('Userpage', () => {
   it('Signs in', () => {
     cy.visit('http://localhost:3000');
     cy.findByText('Login').click();
-    cy.findByLabelText('Username').type(Cypress.env('TEST_USER'));
-    cy.findByLabelText('Password').type(Cypress.env('TEST_PASSWORD'));
+    cy.get('#loginUsername').type(Cypress.env('TEST_USER'));
+    cy.get('#loginPassword').type(Cypress.env('TEST_PASSWORD'));
     cy.get('#loginBtn').click();
   });
   it('Visits userpage', () => {
